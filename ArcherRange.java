dcbd76eeb3a0b73a9fc184f8ded1cc7653645221
Garv Anand <garv.anand187@gmail.com>
2025-10-07 18:41:39 +0530

    Create ArcherRange.java

---
+++ ArcherRange.java
@@ -0,0 +1,16 @@
+public class ArcherRange {
+    public static int findFirstOccurrence(int[] arr, int key) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 2, 2, 3};
+        int key = 2;
+        System.out.println(findFirstOccurrence(arr, key));
+    }
+}
