83c3e5525623ca1158df2a773a5113134237a3bc
Garv Anand <garv.anand187@gmail.com>
2025-10-07 18:44:38 +0530

    Create HiddenChamber.java

---
+++ HiddenChamber.java
@@ -0,0 +1,11 @@
+public class HiddenChamber {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4};
+        System.out.println(sum(arr, 0));
+    }
+
+    static int sum(int[] arr, int i) {
+        if (i == arr.length) return 0;
+        return arr[i] + sum(arr, i + 1);
+    }
+}
